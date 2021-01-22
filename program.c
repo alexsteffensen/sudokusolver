136d43e4b578cc6f85f07a1df28344da93a6f43a
AlexanderSteffensen <asteff20@student.aau.dk>
2021-01-22 12:24:38 +0100

    Program now reads and prints a soduko board

--- program.c
+++ program.c
@@ -21,39 +21,44 @@ int main(void) {
   int i, j = 0;
   FILE *sudoku_data = fopen("input.txt" , "r");
 
-  
+  /*
   for (i = 0; i < NUM_OF_BLOCKS; i++) {
     blocks[i].num = i + 1;
     for(j = 0; j < NUM_OF_NUMS; j++)
       blocks[i].numbers[j] = j + 1;
     printf("HELlO %d \n", blocks[i].numbers[i]);
-  }
+  }*/
 
-  /*read_file(blocks, sudoku_data);*/
+  read_file(blocks, sudoku_data);
 
   print_sudoku(blocks);
 
   return EXIT_SUCCESS;
 }
-/*
+
 void read_file(block *blocks, FILE *data) {
   char line[MAX_LINE_LEN], first_char;
-  int i, j = 0, counter = 0;
+  int i = -1, p = 0, counter = 0, blockcounter = 0, soduko_read = 0, number;
 
-  for (i = 0; i < NUM_OF_LINES; i++) {
-    if (counter > 1)
-      j += 3;
+  while (!soduko_read) {
 
     fgets(line, MAX_LINE_LEN, data);
     sscanf(line, "%c", &first_char);
 
     if (first_char == '|') {
-      sscanf(line, "| %d %d %d | %d %d %d | %d %d %d |",
-             blocks);
+      sscanf(line, "| %d %d %d | %d %d %d | %d %d %d | ", 
+             &blocks[(i*3)].numbers[(p*3)], &blocks[(i*3)].numbers[(p*3)+1], &blocks[(i*3)].numbers[(p*3)+2],     
+             &blocks[(i*3)+1].numbers[(p*3)], &blocks[(i*3)+1].numbers[(p*3)+1], &blocks[(i*3)+1].numbers[(p*3)+2],
+             &blocks[(i*3)+2].numbers[(p*3)], &blocks[(i*3)+2].numbers[(p*3)+1], &blocks[(i*3)+2].numbers[(p*3)+2]);
+      p++;
     }
 
     else if (first_char == '-') {
-      counter++;
+      i++;
+      p = 0;
+      if (i > 2)
+        soduko_read = 1;
+
       continue;
     }
 
@@ -62,14 +67,13 @@ void read_file(block *blocks, FILE *data) {
   }
 
 }
-*/
+
 
 void print_sudoku(block *blocks) {
   int i, p, j, k;
 
   printf("-------------------------\n");
   for(i = 0; i < NUM_OF_BLOCKROWS; i++) {
-    
     for (p = 0; p < NUM_OF_BLOCKS/3; p++){
       for(j = 0; j < NUM_OF_NUMS/3; j++) {
         printf("| ");
